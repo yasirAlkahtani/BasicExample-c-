5689ce7795fe8324ad7830bed7df1659f1a0685d
yasirAlkahtani <52216321+yasirAlkahtani@users.noreply.github.com>
2019-06-26 21:08:08 +0200

    Add files via upload

---
+++ VariableDeclaration.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+   // Variable definition:
+   int V1, V2;
+   int Result;
+   float FloatNumb;
+
+   // actual initialization
+   V1 = 10;
+   V2 = 20;
+   Result = V1 + V2;
+
+   cout << Result << endl ;
+
+   FloatNumb = 70.0/3.0;
+   cout << FloatNumb << endl ;
+
+   return 0;
+}
