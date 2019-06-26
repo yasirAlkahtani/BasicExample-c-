32959c9fa778fced539cd169f09c449e84fa371a
yasirAlkahtani <52216321+yasirAlkahtani@users.noreply.github.com>
2019-06-26 21:28:52 +0200

    Add files via upload

---
+++ functionLoop.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+// Function declaration
+void func(void);
+
+static int count = 20; /* Global variable */
+
+main() {
+
+   while(count--) {
+      func();
+   }
+
+   return 0;
+}
+
+// Function definition
+void func( void ) {
+   static int i = 5; // local static variable
+   i++;
+   std::cout << "i is " << i ;
+   std::cout << " and count is " << count << std::endl;
+}
