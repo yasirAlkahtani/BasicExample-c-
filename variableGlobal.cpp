fa389f7cd4b68189f4f5d754e4a8f55e844d3e06
yasirAlkahtani <52216321+yasirAlkahtani@users.noreply.github.com>
2019-06-26 21:16:03 +0200

    Add files via upload

---
+++ variableGlobal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Global variable declaration:
+int globalVariabel = 20;
+
+int main () {
+   // Local variable declaration:
+   int globalVariabel = 10;
+
+   cout << globalVariabel<<endl;
+
+   return 0;
+}
